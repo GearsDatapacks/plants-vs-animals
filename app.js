171f2c5060bcc28b213a56737c37ff5b6fc33be4
Surya Rose <surya@seriousinfinitude.com>
2019-12-29 12:53:55 +0000

    made sycamore move only when bee is close

--- app.js
+++ app.js
@@ -1,5 +1,98 @@
 const path = document.getElementById('path');
 const style = document.querySelector('style');
+const healthBar = document.getElementById('healthBar');
+const shadowHealth = document.getElementById('shadowHealth');
+const bee = document.getElementById('beemove');
+const sycamore = document.getElementById('sycamore');
+const healthText = document.getElementById('health');
+const sycamoreRange = document.getElementById('range');
+let health = 100;
+let beeStats;
+let sycamoreStats;
+
+fetch('plants.json', {
+    headers: {
+      'Accept': 'application/json'
+    },
+    method: 'GET',
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(onSuccess)
+  .catch(onError);
+
+function onSuccess (plants) {
+  sycamoreStats = plants.sycamore;
+}
+
+function onError (err) {
+  console.error(err);
+  throw err;
+}
+
+fetch('animals.json', {
+    headers: {
+      'Accept': 'application/json'
+    },
+    method: 'GET',
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(onSuccess)
+  .catch(onError);
+
+function onSuccess (animals) {
+  beeStats = animals.bee;
+  sycamoreCheck();
+  changeHealth(0);
+  setHealth();
+  setAnimalAnimation();
+  setPathWidth();
+}
+
+function onError (err) {
+  console.error(err);
+  throw err;
+}
+
+function isTouching (elem1, elem2) {
+  const bbox1 = elem1.getBoundingClientRect();
+  const bbox2 = elem2.getBoundingClientRect();
+  
+  if (bbox2.left >= bbox1.left && bbox2.left <= bbox1.right && bbox2.bottom <= bbox1.bottom && bbox2.bottom >= bbox1.top) {
+    return true;
+  }
+  
+  else if (bbox2.right >= bbox1.left && bbox2.right <= bbox1.right && bbox2.top <= bbox1.bottom && bbox2.top >= bbox1.top) {
+    return true;
+  }
+  
+  else if (bbox1.left <= bbox2.left && bbox1.left >= bbox2.right && bbox1.bottom >= bbox2.bottom && bbox1.bottom <= bbox2.top) {
+    return true;
+  }
+  
+  else if (bbox1.right >= bbox2.left && bbox1.right <= bbox2.right && bbox1.top <= bbox2.bottom && bbox1.top >= bbox2.top) {
+    return true;
+  }
+  
+  else {
+    return false;
+  }
+}
+
+function sycamoreCheck () {
+  if (isTouching(sycamoreRange, bee)) {
+    sycamore.setAttribute('data-moving', 'true');
+  }
+  
+  else {
+    sycamore.setAttribute('data-moving', 'false');
+  }
+  
+  setTimeout(sycamoreCheck, 5);
+}
 
 function setPathWidth () {
   path.setAttribute('d', `M0, 250 q${window.innerWidth / 4}, 100 ${window.innerWidth / 2}, 0 q${window.innerWidth / 4}, -100 ${window.innerWidth / 2}, 0 l0, 50 q-${window.innerWidth / 4}, -100 -${window.innerWidth / 2}, 0 q-${window.innerWidth / 4}, 100 -${window.innerWidth / 2}, 0 z`);
@@ -7,7 +100,7 @@ function setPathWidth () {
 
 function setAnimalAnimation () {
   style.textContent = `g#beemove {
-        animation: 5s linear infinite forwards fly;
+        animation: ${window.innerWidth / 100}s linear infinite forwards fly;
       }
       
       @keyframes fly {
@@ -18,8 +111,8 @@ function setAnimalAnimation () {
         12% {
           transform: translate(${window.innerWidth / 8}px, 50px);
         }
-        
-        87% {
+    
+        80% {
           transform: translate(${window.innerWidth * 0.75}px, -50px);
         }
         
@@ -29,5 +122,18 @@ function setAnimalAnimation () {
       }`;
 }
 
-setAnimalAnimation();
-setPathWidth();
+function setHealth () {
+  healthText.setAttribute('x', `${window.innerWidth / 4 + 5}`);
+  healthBar.setAttribute('x', `${window.innerWidth / 4}`);
+  healthBar.setAttribute('width', `${window.innerWidth / 2}`);
+  shadowHealth.setAttribute('x', `${window.innerWidth / 4 - 2}`);
+  shadowHealth.setAttribute('width', `${window.innerWidth / 2 + 4}`);
+}
+
+function changeHealth (num) {
+  health += num;
+  healthText.textContent = health;
+  healthBar.setAttribute('width', `${window.innerWidth / 200 * health}`);
+}
+
+bee.addEventListener('animationiteration', () => {changeHealth(Number(`-${beeStats.damage}`));});
